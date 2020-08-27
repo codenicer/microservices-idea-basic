1b9afcaf3c9b73cb6969eaa00253c860c17e02ce
string-Nicer <ruthertenido@gmail.com>
2020-08-28 00:55:07 +0800

    use nodemon with babel

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,31 @@
 import express from "express"
+import bodyParser from 'body-parser'
+import {ramdomBytes} from 'crypto'
 import cors from 'cors'
 
-const app = express()
 
-app.get('/post',(req,res)=>{
-     
+const app = express()
+app.use(bodyParser.json())
+//in memory database for testing.
+const post = {};
+
+
+app.get('/posts',(req,res)=>{
+     res.send(post)
 })
 
-app.post('/post',(req,res)=>{
+app.post('/posts',(req,res)=>{
+    const id = ramdomBytes(4).toString('hex')
+    const {
+        title
+    } = req.body
 
+
+    posts[id] = {
+        id,title
+    }
+
+    res.status(201).send(posts[id])
 })
 
 const PORT = process.env.PORT || 5000
@@ -16,3 +33,4 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT , ()=>{
    console.log(`Listening to port ${PORT}`)
 })
+
