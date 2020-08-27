046624ff0756fdfba7f35e502f3b107c51696ecf
string-Nicer <ruthertenido@gmail.com>
2020-08-28 00:41:07 +0800

    setting up ES6 syntax on post-service

--- posts/index.js
+++ posts/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from 'cors'
 
 const app = express()
 
@@ -13,5 +14,5 @@ app.post('/post',(req,res)=>{
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT , ()=>{
-        console.log(`Listening to port ${PORT}`)
+   console.log(`Listening to port ${PORT}`)
 })
