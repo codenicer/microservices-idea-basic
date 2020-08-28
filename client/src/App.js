f5c3f98382c6c356d08fe0d6db3f0015f22db744
string-Nicer <ruthertenido@gmail.com>
2020-08-28 23:12:42 +0800

    add comment list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PostCreate from './PostCreate'
+import PostList from './PostList'
 
 export default ()=>{
     return <div className="container">
-        <h1>Create Post</h1>
+        <h1>Create Postsss</h1>
         <PostCreate />
+        <hr/>
+        <h1>Post</h1>
+        <PostList/>
     </div>
 }
