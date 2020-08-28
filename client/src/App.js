8f038b442cb1481614df4c2ef42579a0d1c4eaad
string-Nicer <ruthertenido@gmail.com>
2020-08-29 01:17:42 +0800

    add comment create and comment list in client

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import PostList from './PostList'
 
 export default ()=>{
     return <div className="container">
-        <h1>Create Postsss</h1>
+        <h1>Create Post</h1>
         <PostCreate />
         <hr/>
         <h1>Post</h1>
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+import CommentCreate from './CommentCreate'
+import CommentList from './CommentList'
 
 export default () =>{
-    return <div>
-        
+   const [posts,setPosts] = useState({})
+
+   const fetchPost = async () =>{
+       const res = await axios.get('http://localhost:5000/posts')
+
+       setPosts(res.data)
+   }
+
+   useEffect(()=>{
+        fetchPost()
+   },[])
+
+   const renderedPost = Object.values(posts).map(post=>{
+       return <div className="card"
+        style={{width:'30%',marginBottom:'20px'}}
+        key={post.id}
+       >
+           <div className='card-body'>
+                <h3>{post.title}</h3>
+                <CommentList postId={[post.id]}/>
+                <CommentCreate postId={post.id}/>
+           </div>
+       </div>
+   })
+    
+    return <div className="d-flex flex-row flex-wrap justify-content-between">
+       {renderedPost}
     </div>
 }
