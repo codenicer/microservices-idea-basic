86024a22def94a27b20aa22a1e22bddbf20e0b2b
string-Nicer <ruthertenido@gmail.com>
2020-08-29 02:13:02 +0800

    add query-service back end

---
+++ query-service/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import  bodyParser  from 'body-parser'
+import  cors from 'cors'
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
+
+
+const PORT = process.env.PORT || 5003
+
+app.listen(PORT , ()=>{
+   console.log(`Listening to port ${PORT}`)
+})
+
